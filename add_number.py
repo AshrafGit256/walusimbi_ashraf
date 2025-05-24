86d2305ca96078d89f5e1edc25b396d6aa70e035
AshrafGit256 <ashrafwalusimbi27@gmail.com>
2025-05-24 15:29:33 +0300

    Made updates in all previous assignments and added the inventory handling assignment too

--- add_number.py
+++ add_number.py
@@ -4,8 +4,17 @@
     
 # add_numbers(1, 2)
 
-def product_of_numbers(x,y):
-    result = x*y
-    print('product:', result)
+# ------- Product of two numbers --------- #
+
+# def product_of_numbers(x,y):
+#     result = x*y
+#     print('product:', result)
     
-product_of_numbers(2, 3)
+# product_of_numbers(2, 3)
+
+# Simple Exe one , Write a function to get product of three values:
+
+def product(x,y,z):
+    result = x*y*z
+    print('Product of three numbers:', result)
+product(1,2,3)
--- lambda_functions.py
+++ lambda_functions.py
@@ -14,8 +14,14 @@
 # add = lambda a, b : a + b
 # print(add(10, 5))
 
-numbers = [1, 2, 3, 4, 5]
-squared = list(map(lambda x: x*x, numbers))
-print(squared)
+# numbers = [1, 2, 3, 4, 5]
+# squared = list(map(lambda x: x*x, numbers))
+# print(squared)
+
 
 # Assignment one : Find the factorial of a number (five)
+
+def factorial_of_5(a,b,c,d,e):
+    factorial = lambda a,b,c,d,e: a*b*c*d*e
+    print(factorial(a,b,c,d,e))
+factorial_of_5(1,2,3,4,5)
